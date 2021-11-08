3a9a88cadbf51b004d9bd23f1901e614712fd3af
Xx_Krostofer_xX <92882782+Krostofer@users.noreply.github.com>
2021-11-08 14:22:13 -0500

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 a_button = 0
+button_a_sound = 0
 lightning_timer = 0
 lightning_distance = 0
 lightning_distance_kilometres = 0
 display_complete = 0
 
 def on_button_pressed_a():
-    global a_button, lightning_timer
+    global a_button, button_a_sound, lightning_timer
     a_button += 1
+    button_a_sound += 1
     while a_button > 0:
         lightning_timer += 1
         basic.pause(1000)
@@ -20,6 +22,7 @@ input.on_button_pressed(Button.AB, on_button_pressed_ab)
 
 def on_button_pressed_b():
     global a_button, lightning_distance, lightning_distance_kilometres, display_complete
+    music.stop_all_sounds()
     a_button = 0
     if lightning_timer > 0:
         basic.show_string("" + str((lightning_timer)))
@@ -67,3 +70,9 @@ def on_gesture_shake():
         basic.show_string("" + str((lightning_distance_kilometres)))
         basic.show_string(" km away")
 input.on_gesture(Gesture.SHAKE, on_gesture_shake)
+
+def on_microbit_id_button_a():
+    music.play_melody("C5 B A G F E D C ", 120)
+control.on_event(EventBusSource.MICROBIT_ID_BUTTON_A,
+    1,
+    on_microbit_id_button_a)
--- main.ts
+++ main.ts
@@ -1,10 +1,12 @@
 let a_button = 0
+let button_a_sound = 0
 let lightning_timer = 0
 let lightning_distance = 0
 let lightning_distance_kilometres = 0
 let display_complete = 0
 input.onButtonPressed(Button.A, function () {
     a_button += 1
+    button_a_sound += 1
     while (a_button > 0) {
         lightning_timer += 1
         basic.pause(1000)
@@ -16,6 +18,7 @@ input.onButtonPressed(Button.AB, function () {
     basic.showString("Press A + B to repeat the lightning distance.")
 })
 input.onButtonPressed(Button.B, function () {
+    music.stopAllSounds()
     a_button = 0
     if (lightning_timer > 0) {
         basic.showString("" + (lightning_timer))
@@ -63,4 +66,7 @@ input.onGesture(Gesture.Shake, function () {
         basic.showString("" + (lightning_distance_kilometres))
         basic.showString(" km away")
     }
+})
+control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_A, 1, function () {
+    music.playMelody("C - - - - - - - ", 120)
 })
